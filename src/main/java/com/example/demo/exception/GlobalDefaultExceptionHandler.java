2b839f885d793205cac07d87b1778e30dc58f3b4
15972112726 <34559392+15972112726@users.noreply.github.com>
2018-05-07 15:26:55 +0800

    9.2 全局异常捕获

---
+++ src/main/java/com/example/demo/exception/GlobalDefaultExceptionHandler.java
@@ -0,0 +1,29 @@
+package com.example.demo.exception;
+
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * 9.2 全局异常捕获
+ * <p>
+ * 如果项目前后端是通过 JSON 进行数据通信，则当出现异常时可以常用如下方式处理异常信息。
+ * <p>
+ * 编写一个类充当全局异常的处理类，需要使用 @ControllerAdvice 和 @ExceptionHandler 注解：
+ * Created by dell on 2018/5/7.
+ */
+@ControllerAdvice
+public class GlobalDefaultExceptionHandler {
+
+    @ExceptionHandler(Exception.class)
+    @ResponseBody
+    public Map<String, Object> defaultExceptionHandler(Exception e) {
+        Map<String, Object> map = new HashMap<String, Object>();
+        map.put("code", 500);
+        map.put("msg", e.getMessage());
+        return map;
+    }
+}
